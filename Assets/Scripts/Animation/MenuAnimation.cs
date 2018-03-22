9e02cfa4741244efde105830103aaf036edbad80
Benjamin Uijldert <bmuijldert@gmail.com>
2018-03-22 12:43:24 +0100

    added really small menu animation

---
+++ Assets/Scripts/Animation/MenuAnimation.cs
@@ -0,0 +1,23 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuAnimation : MonoBehaviour {
+
+    [SerializeField] private Transform[] _animatedObjects;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < _animatedObjects.Length; i++)
+        {
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(_animatedObjects[i].DOScaleY(0, 0f));
+            sequence.AppendInterval(0.2f);
+            sequence.AppendInterval(i * .5f);
+            sequence.Append(_animatedObjects[i].DOScaleY(1, .5f));
+        }
+        
+
+    }
+}
