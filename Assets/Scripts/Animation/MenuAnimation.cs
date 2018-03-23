c2fce1fbdbe226203cbbe3d7cba4d0039c5da67a
Benjamin Uijldert <bmuijldert@gmail.com>
2018-03-23 09:23:13 +0100

    changed menu animation

--- Assets/Scripts/Animation/MenuAnimation.cs
+++ Assets/Scripts/Animation/MenuAnimation.cs
@@ -7,17 +7,18 @@ public class MenuAnimation : MonoBehaviour {
 
     [SerializeField] private Transform[] _animatedObjects;
 
+    private float _animDuration = .25f;
+
     private void OnEnable()
     {
         for (int i = 0; i < _animatedObjects.Length; i++)
         {
             Sequence sequence = DOTween.Sequence();
-            sequence.Append(_animatedObjects[i].DOScaleY(0, 0f));
-            sequence.AppendInterval(0.2f);
-            sequence.AppendInterval(i * .5f);
-            sequence.Append(_animatedObjects[i].DOScaleY(1, .5f));
+            sequence.Append(_animatedObjects[i].DOScale(0, 0f));
+            sequence.AppendInterval(0.25f);
+            sequence.AppendInterval(i * _animDuration);
+            sequence.Append(_animatedObjects[i].DOScale(1.25f, _animDuration));
+            sequence.Append(_animatedObjects[i].DOScale(1, _animDuration));
         }
-        
-
     }
 }
